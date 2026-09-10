9c49f1d6feeed243d1980e0a677b79f1563be04e
HarshTheCreator <hindustaniboy89@gmail.com>
2026-09-10 12:16:10 +0530

    Time: 32 ms (23.84%), Space: 53 MB (8.6%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,20 +1,34 @@
 class Solution {
     public int countSubstrings(String s) {
         int count = 0;
+        int n = s.length();
+        int[][] dp = new int[n+1][n+1];
 
-        for(int i=0; i<s.length(); i++){
-            count += expand(s, i, i);
-            count += expand(s, i, i+1);
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(checkPalindrome(s, i, j, dp)){
+                    count++;
+                }
+            }
         }
         return count;
     }
-    public int expand(String s, int left, int right){
-        int count = 0;
-        while(left >= 0 && right < s.length()  && s.charAt(left) == s.charAt(right)){
-            count++;
-            left--;
-            right++;
+    public boolean checkPalindrome(String s, int left, int right, int[][] dp){
+        if (left >= right) {
+            return true;
         }
-        return count;
+        if (dp[left][right] != -1) {
+            return dp[left][right] == 1;
+        }
+        if (s.charAt(left) != s.charAt(right)) {
+            dp[left][right] = 0;
+            return false;
+        }
+        boolean result = checkPalindrome(s, left + 1, right - 1, dp);
+        dp[left][right] = result ? 1 : 0;
+        return result;
     }
 }
