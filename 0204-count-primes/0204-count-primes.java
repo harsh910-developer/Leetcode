7182e29eec9a3714521f7b101af03d74cf8328a1
HarshTheCreator <hindustaniboy89@gmail.com>
2026-09-17 12:36:50 +0530

    Time: 758 ms (21.73%), Space: 79.7 MB (32.79%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countPrimes(int n) {
         boolean[] prime = new boolean[n];
-        Arrays.fill(prime, false);
+        Arrays.fill(prime, true);
 
         if(n < 2){
             return 0;
@@ -9,14 +9,14 @@ class Solution {
 
         int count = 0;
         for (int i = 2; i * i < n; i++) {
-            if (!prime[i]) {
+            if (prime[i]) {
                 for (int j = i * i; j < n; j += i) {
-                    prime[j] = true;
+                    prime[j] = false;
                 }
             }
         }
         for(int i=2; i<n; i++){
-            if(!prime[i]){
+            if(prime[i]){
                 count++;
             }
         }
